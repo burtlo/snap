e975e44e113e31e52f58ca83153727b1627d55ae
Franklin Webber <franklin.webber@gmail.com>
2017-10-23 22:36:57 -0500

    Adds initial set of snap tools
    Lots of repetition. Lots of problems. This is good.
    
    Signed-off-by: Franklin Webber <franklin@chef.io>

---
+++ plan.sh
@@ -0,0 +1,17 @@
+pkg_name=snap
+pkg_origin=franklinwebber
+pkg_version="0.1.0"
+
+pkg_maintainer="Franklin Webber <franklin.webber@gmail.com>"
+pkg_license=('Apache-2.0')
+
+pkg_bin_dirs=(bin)
+
+do_build() {
+    return 0
+}
+
+do_install() {
+    cp bash/* $pkg_prefix/bin
+    chmod 777 $pkg_prefix/bin/*
+}
